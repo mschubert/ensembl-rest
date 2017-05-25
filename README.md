66f08e92eb1768a6466a5d171163e83017975d3e
mschubert <mschu.dev@gmail.com>
2017-05-25 15:10:14 +0100

    traits and frequencies

--- README.md
+++ README.md
@@ -3,11 +3,12 @@ Ensembl REST API
 
 * [API documentation](http://rest.ensembl.org/)
 * [Course web site](http://training.ensembl.org/events/2017/2017-05-25-API_Cam_May)
+* [Presentation slides (pdf)](http://ftp.ebi.ac.uk/pub/databases/ensembl/training/2017/API_Cam_May/API_course_slide_deck.pdf)
 
 Sections
 --------
 
-* [Basic lookup (core)](basic_lookup)
+* [Core](basic_lookup)
     * Look up a gene symbol, get its FASTA sequence, get all transcripts,
     convert between GRCh38 and GRCh37
     ([gene_lookup.py](basic_lookup/gene_lookup.py))
@@ -18,3 +19,5 @@ Sections
 * [Variation](variation)
     * Fetch variants in region, get its details, check overlap with transcripts
     ([variants.py](variation/variants.py))
+    * Get variants associated with a trait and the frequencies in a population
+    ([coffee.py](variation/coffee.py))
---
+++ variation/coffee.py
@@ -0,0 +1,30 @@
+import sys, json
+sys.path.append("..")
+from helper import fetch_endpoint, fetch_endpoint_POST
+
+# Get all variants that are associated with the phenotype 'Coffee
+# consumption'. For each variant print
+# a. the p-value for the association
+# b. the PMID for the publication which describes the association between
+#    that variant and ‘Coffee consumption’
+# c. the risk allele and the associated gene.
+#
+# Extra: Find the most significant association (lowest p-value) for the
+# phenotype 'coffee consumption'
+
+r1 = fetch_endpoint("phenotype/term/human/Coffee consumption")
+for r in r1:
+    print("{external_reference} {associated_gene} {risk_allele} p={p_value}".format(**r['attributes']))
+
+# Print the allele frequency for the variants’ risk alleles in the Finish
+# population. The name of the population is 1000GENOMES:phase_3:FIN
+
+#r2 = fetch_endpoint("info/variation/populations/human") # 13375 populations
+
+ids = {'ids' : [r['Variation'] for r in r1] }
+ids['pops'] = "1000GENOMES:phase_3:FIN"
+
+r3 = fetch_endpoint_POST("variation/human", data=json.dumps(ids))
+
+for k,v in r3.items():
+    print(k, "\tminor: {minor_allele}\tfreq: {MAF}".format(**v))
