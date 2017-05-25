799af8f9dfcb49b1f0cb6881d5687d60336486c0
mschubert <mschu.dev@gmail.com>
2017-05-25 11:56:36 +0100

    add seq lookup

---
+++ seq_lookup_post.py
@@ -0,0 +1,18 @@
+import sys
+import requests
+import json
+sys.path.append(".")
+from helper import fetch_endpoint, fetch_endpoint_POST
+
+#http://rest.ensembl.org/lookup/symbol/human/IRAK4?content-type=application/json
+
+r1 = fetch_endpoint("lookup/symbol/human/IRAK4")
+print(r1['id'])
+
+r2 = fetch_endpoint("lookup/id/" + r1['id'] + "?expand=1")
+
+transcripts = {'ids' : [t['id'] for t in r2['Transcript']]}
+
+r3 = fetch_endpoint_POST("sequence/id",
+        data = json.dumps(transcripts),
+        content_type="text/x-fasta")
